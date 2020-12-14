489cc44aec08794955c45fdde17eff09a54d9b46
ShowBaba <samwise858@gmail.com>
2020-12-14 16:14:21 +0100

    add cors options

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const logger = require('morgan');
+const cors = require('cors');
+
 
 const app = express();
+const corsOptions = {
+  origin: '*',
+};
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
